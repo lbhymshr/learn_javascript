95716d28832df6e7d494cce9d2570b6aa3ded62f
Alabhya Mishra <lbhymshr@users.noreply.github.com>
2023-09-18 01:09:29 +1000

    Create script copy.js

---
+++ 01-Fundamentals-Part-1/script copy.js
@@ -0,0 +1,28 @@
+/*
+Java script is dynamically typed
+This is the format for block comment
+*/
+
+let js = 'amazing';
+
+let firstName = "Dev"; // use camelCase for variable naming
+console.log(firstName);
+
+let PI = 3.141; // use all upper case for constants!
+console.log('The value of pi is ' + PI);
+
+// use descriptive naming for variables
+let myFirstJob = "Credit Analyst";
+let myCurrentJob = "Data Scientist";
+
+console.log(myFirstJob)
+
+let fullAge = true; // or false
+
+let children; // creating an undefined variable
+
+console.log(fullAge);
+
+console.log(typeof fullAge);
+console.log(typeof myFirstJob);
+console.log(typeof PI);
